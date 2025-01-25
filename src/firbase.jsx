9ddc311f27387d771f9586014680766ac05e1281
Sumit_Zende <sumitzende652@gmail.com>
2025-01-25 06:37:34 +0530

    created firebbase sdk

---
+++ src/firbase.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1100EC2FWp4enpL-Pyb1ix-JAe2JpqqY",
+  authDomain: "loginsystem-94849.firebaseapp.com",
+  projectId: "loginsystem-94849",
+  storageBucket: "loginsystem-94849.firebasestorage.app",
+  messagingSenderId: "810555453804",
+  appId: "1:810555453804:web:9f03a3caff393e242d8349"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
